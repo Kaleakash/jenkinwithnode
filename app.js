bb14eb2796c65de0c87a7c41b7a4937ff2bdb6ff
kaleakash <akash300383@gmail.com>
2021-04-22 19:11:52 +0530

    done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Node JS with Jenking")
+let app = require("express")()
+
+
+app.get("/",(req,res)=> {
+	res.send("Welcome to Express module using docker");
+})
+
+
+app.listen(3000,()=>console.log("Server is running on port number 3000"));
